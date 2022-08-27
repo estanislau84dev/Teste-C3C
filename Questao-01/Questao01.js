29cf058872a4dc2fed03e0d50647125caa213784
estanislau84dev@gmail.com <estanislau84dev@gmail.com>
2022-08-27 14:58:53 -0300

    Adiciona Questao-01

--- Questao-01/Questao01.js
+++ Questao-01/Questao01.js
@@ -1,23 +1,13 @@
-const typeTriangle = (a, b, c) => {
-    if (a < (b + c) && b < (a + c) && c < (a + b)) {
-  
-      if (a == b && b == c) {
-        console.log('Triângulo equilátero');
-      }
-      else if (a == b || a == c || c == b) {
-        console.log('Triângulo isóceles');
-      }
-      else {
-        console.log('Triângulo escaleno');
-      }
-    }
-    else {
-      console.log('Nenhuma medida foi informada!');
-    }
-  
+function checarTipoTriangulo(ladoA, ladoB, ladoC) {
+  let tipoTriangulo
+  if (ladoA == ladoB && ladoB == ladoC) {
+    tipoTriangulo = "equilátero"
+  } else if (ladoA == ladoB || ladoA == ladoC || ladoC == ladoB) {
+    tipoTriangulo = "isosceles"
+
+  } else {
+    tipoTriangulo = "escaleno"
   }
-  
-  typeTriangle(10, 5, 10);
-  typeTriangle(10, 10, 10);
-  typeTriangle(3, 4, 5);
-  typeTriangle();
+
+  return tipoTriangulo
+}
